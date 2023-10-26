afacab77c20920af2ee8803507f3eed4e3c91ee1
André Lima <>
2023-10-26 20:34:02 +0100

    kept the more generic code in utils.py and Terraframe specific code inside Terraframe class

--- terraframe/__init__.py
+++ terraframe/__init__.py
@@ -1,13 +1,11 @@
 from typing import Dict, Any, Type, Optional, Iterable, List, Set
 
-import sys
 import yaml
-import inspect
 from pathlib import Path
 
 from store import TFModelsGlobalStore, get_shared_data_store
 from terraframe.models import TerraFrameBaseModel, DeploymentModel, RemoteStateModel
-from terraframe.utils import yaml_to_dict, exapand_terraframe_templates, create_all_models_from_yaml, get_yaml_key_name_to_models_mapping
+from terraframe.utils import yaml_to_dict, expand_deployment_templates, create_all_models_from_yaml, get_yaml_key_name_to_models_mapping, get_all_matching_files_for_path
 from terraframe.constants import DEPLOYMENT_TEMPLATES_KEY
 
 
@@ -17,60 +15,12 @@ class Terraframe:
     It includes necessary methods to create TerraframeModel objects from a terraframe.yaml file.
     """
     def __init__(self, project_path_str: str, terraframe_yaml_file_name: str = "terraframe.yaml"):
-        print("???")
         self.ds = get_shared_data_store().records
         self.project_path = Path(project_path_str)
         self.terraframe_file = self.project_path / terraframe_yaml_file_name
         self.loaded_dict = self.get_loaded_dict()
-        self.keys_to_models_mapping = self.get_yaml_key_name_to_models_mapping()
+        self.keys_to_models_mapping = get_yaml_key_name_to_models_mapping()
         self.create_all_models_from_yaml(self.loaded_dict, self.keys_to_models_mapping)
-
-    @staticmethod
-    def _expand_deployment_templates(loaded_dict: Dict[str, Any]) -> None:
-        """
-        Helper method that checks if there's deployments to expand in the yaml file.
-        If there's any, it expands it with the specified deployent_template.
-
-        Args:
-            loaded_dict: the yaml file loaded as a python dictionary.
-        """
-        def get_deployment_template(deployment_dict: Dict[str, Any]) -> bool:
-            return deployment_dict.pop("deployment_template", None)
-
-        templates = loaded_dict["deployment_templates"]
-
-        if templates:
-            for deployment in loaded_dict["deployments"]:
-                template_name = get_deployment_template(deployment)
-                if template_name:
-                    deployment.update(templates[template_name])
-
-            loaded_dict.pop("deployment_templates")
-
-    @staticmethod
-    def get_yaml_key_name_to_models_mapping():
-        """
-        Creates a mapping between TerraframeModel '_yaml_directive' attribute and the
-        TerraframeModel class itself. This allows for easy correlation between 'keys'
-        in terraframe.yaml keys and actual TerraframeModels.
-
-        Returns:
-            A dictionary in which each key is a yaml directive and the value is the
-        corresponding TerraframeModel class.
-
-        """
-        model_classes = {}
-        # how do I register modules?
-        # for module in registered_model_modules:
-        for _, obj in inspect.getmembers(sys.modules["terraframe.models"]):
-            try:
-                m_name = getattr(obj, "_yaml_directive").get_default()
-                if inspect.isclass(obj) and m_name:
-                    model_classes[m_name] = obj
-            except AttributeError:
-                continue
-
-        return model_classes
 
     def get_loaded_dict(self) -> Dict[str, Any]:
         """
@@ -79,10 +29,8 @@ class Terraframe:
         Returns:
             The python dictionary representation of the loaded YAML.
         """
-        with open(self.terraframe_file, "r") as f:
-            dictionary = yaml.load(f, Loader=yaml.FullLoader)
-
-        self._expand_deployment_templates(dictionary)
+        dictionary = yaml_to_dict(yaml_file_path=self.terraframe_file)
+        expand_deployment_templates(dictionary)
 
         return dictionary
 
@@ -109,6 +57,80 @@ class Terraframe:
 
             model_cls.factory_for_yaml_data(yaml_dict[yaml_key])
 
+    @staticmethod
+    def _create_maintf_file(deployment: DeploymentModel, deployment_path: Path, main_file_name: Optional[str] = "main.tf") -> None:
+        """
+        Creates the 'main' file inside a deployment directory
+
+        Args:
+            deployment: the DeploymentModel instance.
+            deployment_path: a Path object pointing to the folder for the 'deployment' object.
+            main_file_name: the name for the main file.
+        """
+        with open(f"{deployment_path.absolute()}/{main_file_name}", "w") as main:
+            main.write(deployment.get_rendered_str())
+
+    @staticmethod
+    def _create_variables_file(deployment: DeploymentModel, deployment_path: Path, variable_file_name: Optional[str] = "variables.tf") -> None:
+        """
+        Creates the 'variables' file inside a deployment folder. For that, the code looks at each ChildModuleVarModel
+        of each ChildModuleModeel of the deployment instance.
+
+        Args:
+            deployment: the DeploymentModel instance.
+            deployment_path: a Path object pointing to the folder for the 'deployment' object
+            variable_file_name: the name for the variables file.
+        """
+        for cm in deployment.child_modules:
+            with open(f"{deployment_path.absolute()}/{variable_file_name}", "w") as variables:
+                for cmv in cm.child_module_vars:
+                    variables.write(f"{cmv.get_rendered_str(extra_vars_dict={'prefix': deployment.prefix})}\n\n")
+
+    @staticmethod
+    def get_terraform_variables_from_module(module_path: Path, variables_file_name: str = "variables.tf") -> List[str]:
+        """Given a module path, extract variable names from its variables file
+
+        Args:
+            module_path: the path to the module folder.
+            variables_file_name: the name of the file holding ONLY variable definitions inside the module.
+
+        Returns:
+            A list of strings in which each element is the var name for the root module
+        """
+        with open(f"{module_path}/{variables_file_name}", "r") as vars_tf_file:
+            lines = vars_tf_file.readlines()
+            variables = [
+                line.split('"')[1].strip() for line in lines if line.startswith("variable")
+            ]
+
+        return variables
+
+    def _create_empty_yml_vars_file(self, file_name: str, variables_file_names: Iterable[str]=("variables.tf",)) -> None:
+        """Creates a YAML file with 'file_name' under 'project_path' directory.
+        This file will contain all variables read from files with a name in 'variables_file_name'
+        found under project_path structure. All variables will have null value assigned.
+
+        Args:
+            file_name: the name of the .yml file to be created
+            variables_file_names: An iterable of strings representing the possible variable
+            file names that will be scanned for variables.
+        """
+        module_folders = [folder for folder in self.project_path.iterdir() if folder.is_dir()]
+
+        dict_structure = {}
+
+        for module in module_folders:
+            var_files = get_all_matching_files_for_path(module, variables_file_names)
+            var_names = []
+
+            for var_file in var_files:
+                var_names += self.get_terraform_variables_from_module(module, var_file.name)
+
+            dict_structure[module.name] = {var: None for var in var_names}
+
+        with open(f"{self.project_path}/{file_name}", "w") as yaml_var_file:
+            yaml_var_file.write(yaml.dump(dict_structure, indent=4))
+
     def process_deployments(self):
         """
         This method is a one-stop-shop to take care of:
@@ -128,96 +150,3 @@ class Terraframe:
             self._create_variables_file(deployment, deployment_path)
         else:
             self._create_empty_yml_vars_file(f"{self.project_path.name}_deployment_vars.yaml")
-
-    @staticmethod
-    def _create_maintf_file(deployment: DeploymentModel, deployment_path: Path, main_file_name: Optional[str] = "main.tf") -> None:
-        """
-        Creates the 'main' file inside a deployment directory
-
-        Args:
-            deployment: the DeploymentModel instance.
-            deployment_path: a Path object pointing to the folder for the 'deployment' object.
-            main_file_name: the name for the main file.
-        """
-        with open(f"{deployment_path.absolute()}/{main_file_name}", "w") as main:
-            main.write(deployment.get_rendered_str())
-
-    @staticmethod
-    def _create_variables_file(deployment: DeploymentModel, deployment_path: Path, variable_file_name: Optional[str] = "variables.tf") -> None:
-        """
-        Creates the 'variables' file inside a deployment. For that, the code looks at each ChildModuleVarModel
-        of each ChildModuleModeel of the deployment instance.
-
-        Args:
-            deployment: the DeploymentModel instance.
-            deployment_path: a Path object pointing to the folder for the 'deployment' object
-            variable_file_name: the name for the variables file.
-        """
-        for cm in deployment.child_modules:
-            with open(f"{deployment_path.absolute()}/{variable_file_name}", "w") as variables:
-                for cmv in cm.child_module_vars:
-                    variables.write(f"{cmv.get_rendered_str(extra_vars_dict={'prefix': deployment.prefix})}\n\n")
-
-    def _create_empty_yml_vars_file(self, file_name: str, variables_file_names: Iterable[str]=("variables.tf",)) -> None:
-        """Creates a YAML file with 'file_name' under 'project_path' directory.
-        This file will contain all variables read from files with a name in 'variables_file_name'
-        found under project_path structure. All variables will have null value assigned.
-
-        Args:
-            file_name: the name of the .yml file to be created
-            variables_file_names: An iterable of strings representing the possible variable
-            file names that will be scanned for variables.
-        """
-        module_folders = [folder for folder in self.project_path.iterdir() if folder.is_dir()]
-
-        dict_structure = {}
-
-        for module in module_folders:
-            var_files = self.get_all_matching_files_for_path(module, variables_file_names)
-            var_names = []
-
-            for var_file in var_files:
-                var_names += self.get_all_variables_from_module(module, var_file.name)
-
-            dict_structure[module.name] = {var: None for var in var_names}
-
-        with open(f"{self.project_path}/{file_name}", "w") as yaml_var_file:
-            yaml_var_file.write(yaml.dump(dict_structure, indent=4))
-
-    @staticmethod
-    def get_all_matching_files_for_path(path: Path, file_patterns: Iterable[str]) -> Set[Path]:
-        """Recursivelly search and return all files under 'path' folder that match a pattern in 'file_patterns'.
-
-        Args:
-            path: the root path to start the search from
-            file_patterns: file name patterns to match against
-
-        Returns:
-            A set object containing all files found.
-        """
-        files_to_return = set()
-
-        for pattern in file_patterns:
-            matches = path.rglob(pattern)
-            for m in matches:
-                if m.is_file():
-                    files_to_return.add(m)
-
-        return files_to_return
-
-    @staticmethod
-    def get_all_variables_from_module(module_path: Path, variables_file_name: str = "variables.tf") -> List[str]:
-        """Given a module path, extract variable names from its variables file
-
-        Args:
-            module_path: the path to the module folder.
-            variables_file_name: the name of the file holding ONLY variable definitions inside the module.
-
-        Returns:
-            A list of strings in which each element is the var name for the root module
-        """
-        with open(f"{module_path}/{variables_file_name}", "r") as vars_tf_file:
-            lines = vars_tf_file.readlines()
-            variables = [line.split('"')[1].strip() for line in lines if line.startswith("variable")]
-
-        return variables
--- terraframe/utils.py
+++ terraframe/utils.py
@@ -1,129 +1,24 @@
-import sys
-from typing import List, Set, Dict, Optional, Type
+from typing import List, Set, Dict, Type, Iterable
 
 import yaml
-import json
 import sys
 import inspect
 from pathlib import Path
 from custom_collections import HashableDict
 
 
-def get_all_variables_from_module(
-    module_path: Path, variables_file_name: Optional[str] = "variables.tf"
-) -> List[str]:
-    """Given a module path, extract variable names from its variables file
-
-    Args:
-        module_path (Path): the path to the module folder.
-        variables_file_name (Optional[str]): the name of the file holding ONLY variable definitions inside the module.
-        defaults to 'variables.tf'
-
-    Returns:
-        List[str]: each element is the var name for the root module
-    """
-    with open(f"{module_path}/{variables_file_name}", "r") as vars_tf_file:
-        lines = vars_tf_file.readlines()
-        variables = [
-            line.split('"')[1].strip() for line in lines if line.startswith("variable")
-        ]
-
-    return variables
 
 
-def get_variable_names_for_root_module_based_on_child_module(
-    root_module_var_prefix: str,
-    child_module_path: Path,
-    child_module_var_file_name: Optional[str] = "variables.tf",
-) -> List[str]:
-    """Generates names for terraform variables to be used by a module that inherits from a child module.
-
-    Args:
-        root_module_var_prefix (str): a prefix to be appended to each of the child's module var names
-        child_module_path (Path): the path to the child module folder.
-        child_module_var_file_name: (Optional[str]): the name of the file holding ONLY variable definitions inside the module.
-        defaults to 'variables.tf'
-
-    Returns:
-        List[str]: each element is the var name for the root module
-    """
-    return [
-        f"{root_module_var_prefix}{var_name}"
-        for var_name in get_all_variables_from_module(
-            child_module_path, child_module_var_file_name
-        )
-    ]
-
-
-def create_variables_dot_tf_file_for_root_module_based_on_child_module(
-    root_module_path: Path,
-    root_module_vars_prefix: str,
-    child_module_path: Path,
-    child_module_var_file_name: Optional[str] = "variables.tf",
-) -> None:
-    """Creates a .tf file under root_module_path containing all the same variable definitions
-    as seen in child_module_path/child_module_var_file_name.
-
-    The name of each variable will be prefixed with the root_modules first characters seen before
-    a '_' (underscore) character. For example, if the root_module_path is 'terraform/virtual_colo/dmz_ha_pair',
-    each variable in its variables file will start with 'dmz_'.
-
-    The name of this new file will be the same as child_module_var_file_name.
-
-    NOTE: 'description' and 'default_value' fields are not included.
-
-    Args:
-        root_module_path (Path): the path to the root module folder.
-        root_module_vars_prefix (str): the prefix that should be appended each of the root module's vars.
-        child_module_path (Path): the path to the child module folder.
-        child_module_var_file_name (Optional[str], optional): the name of the file holding ONLY variable definitions inside the module.
-        defaults to 'variables.tf'.
-    """
-
-    with open(f"{root_module_path}/{child_module_var_file_name}", "w") as vars_tf_file:
-        for variable in get_variable_names_for_root_module_based_on_child_module(
-            root_module_vars_prefix, child_module_path
-        ):
-            vars_tf_file.write(f'variable "{variable}"' + "{\n}\n\n")
-
-
-def generate_str_for_root_module_input_vars(
-    root_module_path: Path,
-    root_module_vars_prefix: str,
-    variables_file: Optional[str] = "variables.tf",
-) -> str:
-    """Generates a string that can be copied and pasted into a module's call.
-
-    Args:
-        root_module_path (Path): the path to the root module folder.
-        root_module_vars_prefix (str): the prefix that should be appended each of the root module's vars.
-        variables_file (Optional[str], optional): the name of the file holding ONLY variable definitions inside the module. Defaults to "variables.tf".
-
-    Returns:
-        str: a multiline string. For each variable named <prefix_var_name> in root_module_path/variables_file,
-        appends a new line to a string with format: 'var_name = var.prefix_var_name'
-    """
-
-    return_str = """"""
-    all_vars = get_all_variables_from_module(root_module_path, variables_file)
-    prefix_end_position = len(root_module_vars_prefix)
-
-    for var in all_vars:
-        return_str += f"""\n{var[prefix_end_position:]} = var.{var}"""
-
-    return return_str
-
-
-def get_all_matching_files_for_path(path: Path, file_patterns: List[str]) -> Set[Path]:
+def get_all_matching_files_for_path(path: Path, file_patterns: Iterable[str]) -> Set[Path]:
     """Recursivelly search and return all files under 'path' folder that match a pattern in 'file_patterns'.
 
-    Args:
-        path (Path): the root path to start the search from
-        file_patterns (List[str]): file name patterns to match against
+        Args:
+            path: the root path to start the search from
+            file_patterns: file name patterns to match against
 
-    Returns:
-        Set[Path]: a set of all files found
-    """
+        Returns:
+            A set object containing all files found.
+        """
     files_to_return = set()
 
     for pattern in file_patterns:
@@ -135,48 +30,15 @@ def get_all_matching_files_for_path(path: Path, file_patterns: List[str]) -> Set
     return files_to_return
 
 
-def create_empty_yml_vars_file(
-    project_path: Path,
-    file_name: str,
-    variables_file_names: Optional[List[str]] = ["variables.tf"],
-) -> None:
-    """Creates a YAML file with 'file_name' under 'project_path' directory.
-    This file will contain all variables read from files with a name in 'variables_file_name'
-    found under project_path structure. All variables will have null value assigned.
-
-    Args:
-        project_path (Path): path to a folder that holds terraform modules (subfolders)
-        file_name (str): the name of the .yml file to be created
-        variables_file_names (Optional[List[str]]): A list of possible variable file names that will be scanned for variables.
-        Defaults to ["variables.tf"].
-    """
-    module_folders = [folder for folder in project_path.iterdir() if folder.is_dir()]
-
-    dict_structure = {}
-
-    for module in module_folders:
-        var_files = get_all_matching_files_for_path(module, variables_file_names)
-        var_names = []
-
-        for var_file in var_files:
-            var_names += get_all_variables_from_module(module, var_file.name)
-
-        dict_structure[module.name] = {var: None for var in var_names}
-
-    with open(f"{project_path}/{file_name}", "w") as yaml_var_file:
-        yaml_var_file.write(yaml.dump(dict_structure, indent=4))
-
-
 def yaml_to_dict(yaml_file_path: Path) -> dict:
     """Parses a YAML file into a python dict."""
-    print(yaml_file_path)
     with open(yaml_file_path, "r") as f:
         dictionary = yaml.load(f, Loader=yaml.FullLoader)
 
     return dictionary
 
 
-def exapand_terraframe_templates(loaded_dict: dict) -> None:
+def expand_deployment_templates(loaded_dict: dict) -> None:
     """Takes a parsed YAML file as a dict and expands 'deployment_templates' into 'deployments'."""
 
     def _get_template(deployment_dict: dict) -> bool:
@@ -213,11 +75,20 @@ def convert_nested_dict_to_hashabledict(dict_obj: dict) -> HashableDict:
 
 
 def get_yaml_key_name_to_models_mapping():
+    """
+    Creates a mapping between TerraframeModel '_yaml_directive' attribute and the
+    TerraframeModel class itself. This allows for easy correlation between 'keys'
+    in terraframe.yaml keys and actual TerraframeModels.
+
+    Returns:
+        A dictionary in which each key is a yaml directive and the value is the
+    corresponding TerraframeModel class.
+
+    """
     model_classes = {}
-    # TODO: to allow extensibility, this should be a nested loop.
-    #   - the outer FOR would loop through a list of possible models python modules.
-    #   - the inner FOR is exactly like below
-    for _, obj in inspect.getmembers(sys.modules["models"]):
+    # how do I register modules?
+    # for module in registered_model_modules:
+    for _, obj in inspect.getmembers(sys.modules["terraframe.models"]):
         try:
             m_name = getattr(obj, "_yaml_directive").get_default()
             if inspect.isclass(obj) and m_name:
@@ -226,7 +97,6 @@ def get_yaml_key_name_to_models_mapping():
             continue
 
     return model_classes
-
 
 def create_all_models_from_yaml(
     yaml_dict: dict, key_to_model_mapping: Dict[str, Type["TerraFrameBaseModel"]]
