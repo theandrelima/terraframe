2687d127a48834755e528db6f0b316b778f20eaf
André Lima <>
2023-11-11 17:13:53 +0000

    added TODO

--- terraframe/main.py
+++ terraframe/main.py
@@ -2,6 +2,7 @@ from terraframe import Terraframe
 
 
 if __name__ == "__main__":
-    project_path = "/Users/andrelima/Personal/portfolio_projects/terraframe/tests/projects/my_project_2"
+    # TODO: receive project_path from CLI
+    project_path = "/Users/andrelima/Personal/portfolio_projects/terraframe/tests/projects/my_project"
     t = Terraframe(project_path_str=project_path)
     t.process_deployments()
